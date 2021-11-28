e9e7eb83042e080d277424e32afad8d4053d903a
Mateusz <mateusz2bednarczyk@gmail.com>
2021-11-28 19:24:28 +0100

    Dodanie tabelek do panelu admina

--- Przychodnia/admin.py
+++ Przychodnia/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pacjenci
+from .models import Lekarze
+from .models import Wizyty
 
 # Register your models here.
+
+admin.site.register(Pacjenci)
+admin.site.register(Lekarze)
+admin.site.register(Wizyty)
